28c6a6c20fb14937c08e1c9b1afd560059e6650b
Andrea Censi <censi@mit.edu>
2015-07-23 20:17:41 +0200

    minor

--- unison-to-heli-fault.sh
+++ unison-to-heli-fault.sh
@@ -6,5 +6,5 @@ UPROFILE=unison-to-heli-fault
 mkdir -p ~/.unison/
 rm -f ~/.unison/$UPROFILE.prf
 ln -s $PWD/$UPROFILE.prf ~/.unison/$UPROFILE.prf
-unison $UPROFILE -repeat 2 -force  /Volumes/1506-env_fault/env_fault   $*
+unison $UPROFILE -repeat 2 -force  /Volumes/1506-env_fault/env_fault/src   $*
 
